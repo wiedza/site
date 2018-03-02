012172a55107f7c5be217a21d2e09cc7d9de6f95
Patrick Guillerm <patrickguillerm@gmail.com>
2018-03-02 21:38:53 +0100

    change licence for Apache2

--- src/main/java/org/wiedza/site/exception/ErrorType.java
+++ src/main/java/org/wiedza/site/exception/ErrorType.java
@@ -1,13 +1,9 @@
 /* WIEDZA
-* -----------------
-* GNU GENERAL PUBLIC LICENSE
-*  Version 3, 29 June 2007
-*
-* Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
-* Everyone is permitted to copy and distribute verbatim copies
-* of this license document, but changing it is not allowed.
-*/
-
+ * -----------------
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/
+ */
 package org.wiedza.site.exception;
 
 import java.io.Serializable;
--- src/main/java/org/wiedza/site/tools/Loggers.java
+++ src/main/java/org/wiedza/site/tools/Loggers.java
@@ -1,3 +1,9 @@
+/* WIEDZA
+ * -----------------
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/
+ */
 package org.wiedza.site.tools;
 
 import org.slf4j.Logger;
